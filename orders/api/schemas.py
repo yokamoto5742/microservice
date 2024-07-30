1b724aeace31bf708d33d4650f6cfe3c9d461de6
yokamoto5742-h <yokamoto5742@gmail.com>
2024-07-30 18:42:59 +0900

    新規登録

---
+++ orders/api/schemas.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, conint, conlist, validator
+
+
+class Size(Enum):
+    small = 'small'
+    medium = 'medium'
+    big = 'big'
+
+
+class StatusEnum(Enum):
+    created = 'created'
+    paid = 'paid'
+    progress = 'progress'
+    cancelled = 'cancelled'
+    dispatched = 'dispatched'
+    delivered = 'delivered'
+
+
+class OrderItemSchema(BaseModel):
+    product: str
+    size: Size
+    quantity: Optional[conint(ge=1, strict=True)] = 1
+
+    @validator('quantity')
+    def quantity_non_nullable(cls, value):
+        assert value is not None, 'quantity is may be None'
+        return value
+
+
+class CreateOrderSchema(BaseModel):
+    order: conlist(OrderItemSchema)
+
+
+class GetOrderSchema(BaseModel):
+    id: UUID
+    status: StatusEnum
+    created: datetime
+
+
+class GetOrdersSchema(BaseModel):
+    orders: List[GetOrderSchema]

b1db3e96d091abce7caa86fe216312b512281ae4
yokamoto5742-h <yokamoto5742@gmail.com>
2024-07-30 19:21:41 +0900

    api更新

--- orders/api/api.py
+++ orders/api/api.py
@@ -1,48 +1,51 @@
+import uuid
 from datetime import datetime
 from http import HTTPStatus
 from uuid import UUID
 
+from fastapi import HTTPException
 from starlette.responses import Response
 from starlette import status
 
 from orders.app import app
-from orders.api.schemas import CreateOrderSchema
+from orders.api.schemas import CreateOrderSchema, GetOrderSchema, GetOrdersSchema
 
-orders = {
-    'id': 'ff0f1355-e821-4178-9567-550dec27a373',
-    'status': 'delivered',
-    'created': datetime.utcnow(),
-    'orders': [
-        {
-            'product': 'cappuccino',
-            'size': 'medium',
-            'quantity': 1,
-        }
-    ]
-}
+ORDERS =[]
 
 
-@app.get('/orders')
+@app.get('/orders', response_model=GetOrdersSchema)
 def get_orders():
-    return {'orders': [orders]}
+    return {'orders': [ORDERS]}
 
 
-@app.post('/orders', status_code=status.HTTP_201_CREATED)
+@app.post('/orders', status_code=status.HTTP_201_CREATED, response_model=GetOrderSchema)
 def create_order(order_details: CreateOrderSchema):
-    return orders
+    order = order_details.dict()
+    order['id'] = uuid.uuid4()
+    order['created'] = datetime.utcnow()
+    order['status'] = 'created'
+    ORDERS.append(order)
+    return order
 
 
-@app.get('/orders/{order_id}')
+@app.get('/orders/{order_id}', response_model=GetOrderSchema)
 def get_order(order_id: UUID):
-    return orders
+    for order in ORDERS:
+        if order['id'] == order_id:
+            return order
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND.value, detail=f'Order with ID {order_id} not found')
 
 
-@app.put('/orders/{order_id}')
+@app.put('/orders/{order_id}', response_model=GetOrderSchema)
 def update_order(order_id: UUID, order_details: CreateOrderSchema):
-    return orders
+    for order in ORDERS:
+        if order['id'] == order_id:
+            order.update(order_details.dict())
+            return order
+    raise HTTPException(status_code=HTTPStatus.NOT_FOUND.value, detail=f'Order with ID {order_id} not found')
 
 
-@app.delete('/orders/{order_id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/orders/{order_id}', status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
 def delete_order(order_id: UUID):
     return Response(status_code=HTTPStatus.NO_CONTENT.value)
 
